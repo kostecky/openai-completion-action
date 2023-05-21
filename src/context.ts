430ad79ac8bd91ded769406f61e3297964a0f048
Kristian Kostecky <kris@vmfarms.com>
2023-05-20 21:16:13 -0400

    Stringify twice for Github only on the message

--- src/context.ts
+++ src/context.ts
@@ -72,8 +72,14 @@ const run = async (): Promise<void> => {
   core.info(`Request using model: ${payload.model}\n${JSON.stringify(payload, null, 2)}`);
   const response = await openai.createChatCompletion(payload);
   const data = response.data;
-  // Must run JSON.stringify twice to get the stringified JSON to be properly escaped for GitHub Actions
-  core.setOutput('response', JSON.stringify(JSON.stringify(data)));
+  // Must run JSON.stringify on the message
+  data.choices = data.choices?.map((choice) => {
+    if (choice.message?.content) {
+      choice.message.content = JSON.stringify(choice.message.content);
+    }
+    return choice;
+  });
+  core.setOutput('response', JSON.stringify(data));
 };
 
 export default run;
